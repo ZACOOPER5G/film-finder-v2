cfcf3f7ecb48b2142bdc4badc403c50462bbaf69
Zac Cooper <zacooper5g@outlook.com>
2022-11-22 13:11:58 +1100

    added header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,21 @@
-import { Col, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import arrow from "../assets/arrow.gif"
 
 export const Header = () => {
     return (
-        <Row>
-            <Col>
-                <h2>Find a movie</h2>
-                <img src={ arrow } />
-            </Col>
-            <Col>
-                <h2>Find a show</h2>
-                <img src={ arrow } />
-            </Col>
-        </Row>
+        <Container fluid className="bg-dark p-4" >
+            <Row className="row align-items-center justify-content-space-between" >
+                <Col className="find-header m-auto" xs={4} sm={4} lg={4} md={4} >
+                    <h2>Find a movie</h2>
+                    <img className="img-fluid" src={ arrow } />
+                    <button className="header-button">Movies</button>
+                </Col>
+                <Col className="find-header m-auto" xs={4} sm={4} lg={4} md={4}  >
+                    <h2>Find a show</h2>
+                    <img className="img-fluid" src={ arrow } />
+                    <button className="header-button">Shows</button>
+                </Col>
+            </Row>
+        </Container>
     )
 }
