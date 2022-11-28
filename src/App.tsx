d1df53d1a2f4954c13295da07f7a21099799a482
Zac Cooper <zacooper5g@outlook.com>
2022-11-28 12:37:11 +1100

    added loading state

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { Navbar } from './components/Navbar';
 import { Header } from './components/Header';
 import { FilmForm } from './components/FilmForm';
 import { useState } from 'react';
+import { FilmPoster } from './components/FilmPoster';
+import { Spinner } from 'react-bootstrap';
 
 function App() {
   const [movie, setMovie] = useState(false);
   const [show, setShow] = useState(false);
-  const [chosenFilm, setChosenFilm] = useState({});
+  const [chosenFilm, setChosenFilm] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="App">
@@ -17,8 +20,24 @@ function App() {
         {
           // @ts-ignore
           (movie || show) && (
-              <FilmForm movie={movie} show={show} setChosenFilm={setChosenFilm} />
+              <FilmForm setMovie={setMovie} movie={movie} setShow={setShow} show={show} setChosenFilm={setChosenFilm} setLoading={setLoading} />
           ) 
+        }
+        {
+          loading && (
+            <div className="loading" >
+              {/* @ts-ignore */}
+              <h2>Loading selection</h2>
+              <Spinner animation="grow" variant="light" />
+              <Spinner animation="grow" variant="light"  />
+              <Spinner animation="grow" variant="light"  />
+            </div>
+          )
+
+        }
+        {
+          // @ts-ignore
+          chosenFilm && <FilmPoster chosenFilm={chosenFilm} />
         }
     </div>
   );
